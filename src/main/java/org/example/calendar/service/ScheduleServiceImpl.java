76233466e2b8be162189cdb111a42d89139b3ef3
tgg <qazplmtfgcv123@gmail.com>
2025-01-24 11:08:39 +0900

    feat: saveSchedule(Schedule schedule) 메서드 추가

--- src/main/java/org/example/calendar/service/ScheduleServiceImpl.java
+++ src/main/java/org/example/calendar/service/ScheduleServiceImpl.java
@@ -5,6 +5,8 @@ import org.example.calendar.entity.Schedule;
 import org.example.calendar.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class ScheduleServiceImpl implements ScheduleService {
@@ -14,6 +16,11 @@ public class ScheduleServiceImpl implements ScheduleService {
     @Override
     public Schedule saveSchedule(Schedule schedule) {
         return scheduleRepository.saveSchedule(schedule);
+    }
+
+    @Override
+    public Optional<Schedule> findScheduleById(Long id) {
+        return scheduleRepository.findScheduleById(id);
     }
 
 }
