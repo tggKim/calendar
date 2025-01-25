a29a862ed1e2f9a2637a1825bba44aae00a88693
tgg <qazplmtfgcv123@gmail.com>
2025-01-25 15:31:32 +0900

    feat:  User 클래스 생성
    테이블 user에 대한 클래스를 생성

---
+++ src/main/java/org/example/calendar/entity/User.java
@@ -0,0 +1,11 @@
+package org.example.calendar.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private Long userId;
+    private String username;
+    private String email;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+}
