f4f1eae5de7fc3ff278840bdb22740404970069e
tgg <qazplmtfgcv123@gmail.com>
2025-01-27 11:43:55 +0900

    feat: UserRepository 테스트 코드 완성
    userId로 username 찾는 테스트 코드와 username 업데이트하는 테스트 코드 작성

--- src/main/java/org/example/calendar/service/user/UserService.java
+++ src/main/java/org/example/calendar/service/user/UserService.java
@@ -5,6 +5,8 @@ import org.example.calendar.entity.User;
 public interface UserService {
     User saveUser(User user);
 
+    User findUserById(Long userId);
+
     String getUsernameByUserId(Long userId);
 
     void updateUsername(Long userId, String username);
