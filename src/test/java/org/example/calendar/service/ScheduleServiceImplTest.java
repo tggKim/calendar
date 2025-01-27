6c7c2749d9585ca361fae122f01dd4f360e9bf87
tgg <qazplmtfgcv123@gmail.com>
2025-01-27 10:43:45 +0900

    fix: ScheduleService 테스트 코드 수정
    테스트 코드를 수정한 ScheduleService 기반으로 수정했다.

--- src/test/java/org/example/calendar/service/ScheduleServiceImplTest.java
+++ src/test/java/org/example/calendar/service/ScheduleServiceImplTest.java
@@ -2,6 +2,8 @@ package org.example.calendar.service;
 
 import org.assertj.core.api.Assertions;
 import org.example.calendar.entity.Schedule;
+import org.example.calendar.entity.User;
+import org.example.calendar.repository.user.UserRepository;
 import org.example.calendar.service.schedule.ScheduleService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,106 +17,187 @@ import java.util.NoSuchElementException;
 class ScheduleServiceImplTest {
     @Autowired
     ScheduleService service;
+    @Autowired
+    UserRepository userRepository;
 
-//    @Test
-//    @Transactional
-//    void serviceSaveTest(){
-//        Schedule schedule = Schedule.builder()
-//                .todo("할일 테스트")
-//                .username("테스트 유저")
-//                .password("testPassword")
-//                .build();
-//
-//        Schedule savedSchedule = service.saveSchedule(schedule);
-//        Assertions.assertThat(savedSchedule.getTodo()).isEqualTo("할일 테스트");
-//        Assertions.assertThat(savedSchedule.getUsername()).isEqualTo("테스트 유저");
-//    }
-//
-//    @Test
-//    @Transactional
-//    void serviceFindByIdTest(){
-//        Schedule schedule = Schedule.builder()
-//                .todo("할일 테스트")
-//                .username("테스트 유저")
-//                .password("testPassword")
-//                .build();
-//
-//        Schedule savedSchedule = service.saveSchedule(schedule);
-//
-//        Schedule findSchedule = service.findScheduleById(savedSchedule.getId());
-//        Assertions.assertThat(findSchedule.getId()).isEqualTo(savedSchedule.getId());
-//        Assertions.assertThat(findSchedule.getTodo()).isEqualTo("할일 테스트");
-//        Assertions.assertThat(findSchedule.getUsername()).isEqualTo("테스트 유저");
-//        Assertions.assertThat(findSchedule.getCreatedDate()).isEqualTo(savedSchedule.getCreatedDate());
-//        Assertions.assertThat(findSchedule.getUpdatedDate()).isEqualTo(savedSchedule.getUpdatedDate());
-//
-//        // 없는 id 조회
-//        Assertions.assertThatThrownBy(() -> service.findScheduleById(Long.MAX_VALUE)).isInstanceOf(NoSuchElementException.class);
-//    }
-//
-//    @Test
-//    void findAllScheduleTest(){
-//        List<Schedule> schedules = service.findAllSchedule(null,null,null);
-//        for(Schedule schedule : schedules){
-//            System.out.println(schedule.getId()+" "+schedule.getTodo()+" "+schedule.getUsername()+" "+schedule.getCreatedDate()+" "+schedule.getUpdatedDate());
-//        }
-//    }
-//
-//    @Test
-//    @Transactional
-//    void serviceUpdateTest(){
-//        Schedule schedule = Schedule.builder()
-//                .todo("할일 테스트")
-//                .username("테스트 유저")
-//                .password("testPassword")
-//                .build();
-//
-//        Schedule savedSchedule = service.saveSchedule(schedule);
-//
-//        Schedule updateSchedule = Schedule.builder()
-//                .id(savedSchedule.getId())
-//                .todo("할 일 업데이트")
-//                .username("이름 업데이트")
-//                .password(schedule.getPassword())
-//                .build();
-//
-//        Schedule updatedSchedule = service.updateSchedulesTodoAndUsername(updateSchedule);
-//
-//        Assertions.assertThat(updatedSchedule.getId()).isEqualTo(savedSchedule.getId());
-//        Assertions.assertThat(updatedSchedule.getTodo()).isEqualTo("할 일 업데이트");
-//        Assertions.assertThat(updatedSchedule.getUsername()).isEqualTo("이름 업데이트");
-//        Assertions.assertThat(updatedSchedule.getCreatedDate()).isEqualTo(savedSchedule.getCreatedDate());
-//        Assertions.assertThat(updatedSchedule.getUpdatedDate()).isEqualTo(savedSchedule.getUpdatedDate());
-//
-//        // 틀린 비밀번호 예외 확인
-//        Schedule incorrectPasswordSchedule = Schedule.builder()
-//                .id(savedSchedule.getId())
-//                .todo("할 일 업데이트")
-//                .username("이름 업데이트")
-//                .password("틀린 비밀번호")
-//                .build();
-//        Assertions.assertThatThrownBy(() -> service.updateSchedulesTodoAndUsername(incorrectPasswordSchedule)).isInstanceOf(IllegalArgumentException.class);
-//
-//        // 삭제 한 뒤 예외 확인
-//        service.deleteScheduleById(updatedSchedule.getId(), schedule.getPassword());
-//        Assertions.assertThatThrownBy(() -> service.updateSchedulesTodoAndUsername(updateSchedule)).isInstanceOf(NoSuchElementException.class);
-//    }
-//
-//    @Test
-//    void serviceDeleteTest(){
-//        Schedule schedule = Schedule.builder()
-//                .todo("할일 테스트")
-//                .username("테스트 유저")
-//                .password("testPassword")
-//                .build();
-//
-//        Schedule savedSchedule = service.saveSchedule(schedule);
-//
-//        // 잚못된 비밀번호 테스트
-//        Assertions.assertThatThrownBy(() -> service.deleteScheduleById(savedSchedule.getId(), "틀린 비밀번호")).isInstanceOf(IllegalArgumentException.class);
-//
-//        // 잘못된 id 테스트
-//        service.deleteScheduleById(savedSchedule.getId(), schedule.getPassword());
-//        Assertions.assertThatThrownBy(() -> service.deleteScheduleById(savedSchedule.getId(), schedule.getPassword())).isInstanceOf(NoSuchElementException.class);
-//    }
+    // 저장 테스트
+    @Test
+    @Transactional
+    void serviceSaveTest(){
+        User user = User.builder()
+                .username("테스트 유저")
+                .email("rl123456@hanmail.net")
+                .build();
+
+        User savedUser = userRepository.saveUser(user);
+
+        Schedule schedule = Schedule.builder()
+                .todo("할일 테스트")
+                .userId(savedUser.getUserId())
+                .password("testPassword")
+                .build();
+
+        Schedule savedSchedule = service.saveSchedule(schedule);
+
+        Schedule findSchedule = service.findScheduleById(savedSchedule.getId());
+
+        Assertions.assertThat(savedSchedule.getId()).isEqualTo(findSchedule.getId());
+        Assertions.assertThat(savedSchedule.getTodo()).isEqualTo(findSchedule.getTodo());
+        Assertions.assertThat(savedSchedule.getUserId()).isEqualTo(findSchedule.getUserId());
+        Assertions.assertThat(savedSchedule.getCreatedDate()).isEqualTo(findSchedule.getCreatedDate());
+        Assertions.assertThat(savedSchedule.getUpdatedDate()).isEqualTo(findSchedule.getUpdatedDate());
+    }
+
+    // 단건 조회 테스트
+    @Test
+    @Transactional
+    void serviceFindByIdTest(){
+
+        User user = User.builder()
+                .username("테스트 유저")
+                .email("rl123456@hanmail.net")
+                .build();
+
+        User savedUser = userRepository.saveUser(user);
+
+
+        Schedule schedule = Schedule.builder()
+                .todo("할일 테스트")
+                .userId(savedUser.getUserId())
+                .password("testPassword")
+                .build();
+
+        Schedule savedSchedule = service.saveSchedule(schedule);
+
+        Schedule findSchedule = service.findScheduleById(savedSchedule.getId());
+
+        Assertions.assertThat(findSchedule.getId()).isEqualTo(savedSchedule.getId());
+        Assertions.assertThat(findSchedule.getTodo()).isEqualTo("할일 테스트");
+        Assertions.assertThat(findSchedule.getUsername()).isEqualTo("테스트 유저");
+        Assertions.assertThat(findSchedule.getCreatedDate()).isEqualTo(savedSchedule.getCreatedDate());
+        Assertions.assertThat(findSchedule.getUpdatedDate()).isEqualTo(savedSchedule.getUpdatedDate());
+
+    }
+
+    // 전체 조회 테스트
+    @Test
+    void findAllScheduleTest(){
+        List<Schedule> schedules = service.findAllSchedule(null,null,null);
+        for(Schedule schedule : schedules){
+            System.out.println(schedule.getId()+" "+schedule.getTodo()+" "+schedule.getUserId()+" "+schedule.getUsername()+" "+schedule.getCreatedDate()+" "+schedule.getUpdatedDate());
+        }
+    }
+
+    // todo 업데이트 테스트
+    @Test
+    @Transactional
+    void serviceUpdateTest(){
+        User user = User.builder()
+                .username("테스트 유저")
+                .email("rl123456@hanmail.net")
+                .build();
+
+        User savedUser = userRepository.saveUser(user);
+
+        Schedule schedule = Schedule.builder()
+                .todo("할일 테스트")
+                .userId(savedUser.getUserId())
+                .password("testPassword")
+                .build();
+
+        Schedule savedSchedule = service.saveSchedule(schedule);
+
+        try{
+            Thread.sleep(1000);
+        }catch (Exception e){
+
+        }
+
+        service.updateSchedulesTodo(savedSchedule.getId(), "할일 업데이트");
+
+        Schedule findSchedule = service.findScheduleById(savedSchedule.getId());
+
+        Assertions.assertThat(findSchedule.getId()).isEqualTo(savedSchedule.getId());
+        Assertions.assertThat(findSchedule.getTodo()).isEqualTo("할일 업데이트");
+        Assertions.assertThat(findSchedule.getUsername()).isEqualTo("테스트 유저");
+        Assertions.assertThat(findSchedule.getCreatedDate()).isEqualTo(savedSchedule.getCreatedDate());
+        Assertions.assertThat(findSchedule.getUpdatedDate()).isNotEqualTo(savedSchedule.getUpdatedDate());
+
+    }
+
+    // 삭제 테스트
+    @Test
+    @Transactional
+    void serviceDeleteTest(){
+        User user = User.builder()
+                .username("테스트 유저")
+                .email("rl123456@hanmail.net")
+                .build();
+
+        User savedUser = userRepository.saveUser(user);
+
+        Schedule schedule = Schedule.builder()
+                .todo("할일 테스트")
+                .userId(savedUser.getUserId())
+                .password("testPassword")
+                .build();
+
+        Schedule savedSchedule = service.saveSchedule(schedule);
+
+        service.deleteScheduleById(savedSchedule.getId());
+
+        Assertions.assertThatThrownBy(() -> service.findScheduleById(savedSchedule.getId())).isInstanceOf(NoSuchElementException.class);
+    }
+
+    // userId 가져오기 테스트
+    @Test
+    @Transactional
+    void serviceGetUserIdTest(){
+        User user = User.builder()
+                .username("테스트 유저")
+                .email("rl123456@hanmail.net")
+                .build();
+
+        User savedUser = userRepository.saveUser(user);
+
+        Schedule schedule = Schedule.builder()
+                .todo("할일 테스트")
+                .userId(savedUser.getUserId())
+                .password("testPassword")
+                .build();
+
+        Schedule savedSchedule = service.saveSchedule(schedule);
+
+        Long findUserId = service.getUserIdById(savedSchedule.getId());
+
+        Assertions.assertThat(findUserId).isEqualTo(savedUser.getUserId());
+    }
+
+    // 비밀번호 검증 테스트
+    @Test
+    @Transactional
+    void validatePasswordTest(){
+        User user = User.builder()
+                .username("테스트 유저")
+                .email("rl123456@hanmail.net")
+                .build();
+
+        User savedUser = userRepository.saveUser(user);
+
+        Schedule schedule = Schedule.builder()
+                .todo("할일 테스트")
+                .userId(savedUser.getUserId())
+                .password("testPassword")
+                .build();
+
+        Schedule savedSchedule = service.saveSchedule(schedule);
+
+        Assertions.assertThat(service.validatePassword(savedSchedule.getId(), schedule.getPassword())).isTrue();
+
+        Assertions.assertThatThrownBy(() -> service.validatePassword(savedSchedule.getId(), "틀린 비밀번호")).isInstanceOf(IllegalArgumentException.class);
+
+        service.deleteScheduleById(savedSchedule.getId());
+
+        Assertions.assertThatThrownBy(() -> service.validatePassword(savedSchedule.getId(), schedule.getPassword())).isInstanceOf(NoSuchElementException.class);
+    }
 }
